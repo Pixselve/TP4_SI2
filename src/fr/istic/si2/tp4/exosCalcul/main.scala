8c0d6937cc2a22ff1755a7c23111679fbbeaabe5
Mael Kerichard <pixselve@gmail.com>
2020-01-28 09:44:51 +0100

    Exercice 6 (*). Programmez une fonction récursive copie, qui étant donné un entier positif ou nul n et une chaîne s, renvoie la chaîne composée de n fois la chaîne s, en utilisant le tiret comme séparateur
    Exercice 7 (*). Programmez une fonction récursive repeat, qui étant donné un entier positif ou nul n et un caractère c, affiche n fois le caractère c dans la console, avec un retour à la ligne entre chaque caractère.
    
    Exercice 8. Programmez une fonction récursive countDown, qui étant donné un entier positif ou nul n, affiche dans la console le décompte en partant de n jusqu’à 1, en finissant par "Time␣OUT␣!", avec un retour à la ligne entre chaque nombre.

--- src/fr/istic/si2/tp4/exosCalcul/main.scala
+++ src/fr/istic/si2/tp4/exosCalcul/main.scala
@@ -10,22 +10,41 @@ object ExosCalculs extends App {
    * @param s une chaîne
    * @return la chaîne composée de n fois la chaîne s, séparée à chaque fois par un tiret
    */
-  // TODO 
-  def copie(n: Int, s: String): String = ???
+  def copie(n: Int, s: String): String = {
+    n match {
+      case 0 => ""
+      case 1 => s
+      case _ => s + "-" + copie(n - 1, s)
+    }
+  }
 
   /** Affiche n fois le caractère c, en allant à la ligne entre chaque caractère
+   *
    * @param n un entier positif ou nul
    * @param c un caractère
    */
-  // TODO 
-  def repeat(n: Int, c: Char): Unit = ???
+  def repeat(n: Int, c: Char): Unit = {
+    n match {
+      case 0 =>
+      case _ => println(c); repeat(n - 1, c)
+    }
+  }
+
+//  repeat(2, 'c')
 
   /**
    * Affiche le décompte en partant de n, en finissant par "Time OUT !", avec un retour à la ligne entre chaque nombre
+   *
    * @param n un entier positif ou nul
    */
   // TODO 
-  def countDown(n: Int): Unit = ???
+  def countDown(n: Int): Unit = {
+    n match {
+      case 0 => println("Time OUT !")
+      case value => println(value); countDown(n - 1)
+    }
+  }
+  countDown(10)
 
   /**
    * @param n un entier strictement positif
